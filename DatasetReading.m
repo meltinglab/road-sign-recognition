2846041dc691e9a3007f80e3ab0a90589fe628d1
DVandi <57406400+DVandi@users.noreply.github.com>
2020-02-29 14:21:31 +0100

    Dataset reading update
    Transformed the dataset reading script in a function to be called from the main

--- DatasetReading.m
+++ DatasetReading.m
@@ -2,18 +2,19 @@
 %Module Title: Dataset Reading
 %Authors: Damiano Vandi, Matteo Lenti, Filippo Cenonfolo
 
+function [resizedImdsTrain, resizedImdsValidation] = DatasetReading(dataset_path)
+    %dataset_path = fullfile('C:\','Users','Daminao','Desktop','GTSRB','Final_Training','Images');
+    %Class for the management of the dataset
+    imds = imageDatastore(dataset_path, 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+    %Number of images in the dataset
+    %nImages = size(imds.Files,1);
+    %Size of the images
+    imSize = [32 32];
 
-GTSRB_training_path = fullfile('C:\','Users','Daminao','Desktop','GTSRB','Final_Training','Images');
-%Class for the management of the dataset
-imds = imageDatastore(GTSRB_training_path, 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
-%Number of images in the dataset
-nImages = size(imds.Files,1);
-%Size of the images
-imSize = [32 32];
+    %Split dataset: 80% training, 20% validation
+    [imdsTrain,imdsValidation] = splitEachLabel(resizedImds, 0.8, 'randomize');
 
-%Split dataset: 80% training, 20% validation
-[imdsTrain,imdsValidation] = splitEachLabel(resizedImds, 0.8, 'randomize');
-
-%Resize training and validation dataset
-resizedImdsTrain = augmentedImageDatastore(imSize, imdsTrain);
-resizedImdsValidation = augmentedImageDatastore(imSize, imdsValidation);
+    %Resize training and validation dataset
+    resizedImdsTrain = augmentedImageDatastore(imSize, imdsTrain);
+    resizedImdsValidation = augmentedImageDatastore(imSize, imdsValidation);
+end

34c7f1ce06ee8e5abc6a607a4c5c30a6afa9ad7e
DVandi <57406400+DVandi@users.noreply.github.com>
2020-03-02 18:03:45 +0100

    Test script minor updates
    Fixed a bug and added some comments to the test script

--- NetworkTest.m
+++ NetworkTest.m
@@ -1,11 +1,14 @@
-%Description: Script for the network testing
+%Project Title: Road Sign Recognition
+%Module Title: Network Test
+%Description: script for the Network testing
 %Authors: Damiano Vandi, Matteo Lenti, Filippo Cenonfolo
 
+%Load the trained network
 load('trained_net.mat');
 
 %Path of the test images
-dataset_path = 'C:\Users\filip\Downloads\GTSRB_Final_Test_Images\GTSRB\Final_Test\Images'; 
-%Name of the .csv file that contains the GT labels
+dataset_path = 'C:\Users\Daminao\Desktop\GTSRB\Test_PNG'; 
+%Name of the .csv file that contains the Ground Truth labels
 labelsFilename = strcat(dataset_path,'\GT-final_test.csv');
 
 %.csv file loading in a table
@@ -24,7 +27,7 @@ resizedImdsTest = augmentedImageDatastore(imSize, imdsTest);
 
 %Classification and Ground Truth Values
 YPred = classify(net,resizedImdsTest);
-YGT = resizedImdsTest.Labels;
+YGT = imdsTest.Labels;
 
 %Accuracy computation
 accuracy = sum(YPred == YGT)/numel(YGT)

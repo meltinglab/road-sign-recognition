edea20f4cfc41669f94179c12b441ba68c345647
MatteoLenti <57560650+MatteoLenti@users.noreply.github.com>
2020-03-09 15:38:00 +0100

    Code reorganization
    Divided scripts into folders RaspberryDeployment and mResNetCNNConfiguration.

--- RaspberryDeployment/PILRaspberryTest.m
+++ RaspberryDeployment/PILRaspberryTest.m
@@ -3,12 +3,12 @@
 %Description: Script for the PIL testing of the Raspberry
 %Authors: Damiano Vandi, Matteo Lenti, Filippo Cenonfolo
 
-images = dir('TestImages\*.png');
+images = dir('..\TestImages\*.png');
 N = length(images);
 
 % loop for each input image 
 for i = 1:N
-    im = imread(['TestImages\' images(i).name]);
+    im = imread(['..\TestImages\' images(i).name]);
     
     %Classify using mResNet implemented on Raspberry Pi
     outimg = mResNet_classify_pil(double(im));
